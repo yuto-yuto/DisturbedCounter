25a1fa4ff96131de3e54ec670b153ba61b8488ac
yuto-yuto <yuto.nakatsu@gmail.com>
2020-01-03 13:17:11 +0100

    Add button disable/enable function

--- src/MainDisplay.py
+++ src/MainDisplay.py
@@ -17,17 +17,21 @@ class MainDisplay:
         self.window.geometry(f"{width}x{height}")
 
     def start(self):
-        self.__add_disturbed_button()
+        self.__add_button()
         self.__add_history_display_area()
         self.window.mainloop()
 
-    def __add_disturbed_button(self):
+    def __add_button(self):
         topFrame = tkinter.Frame(self.window)
         topFrame.pack()
 
-        button = tkinter.Button(topFrame, text = u"Being disturbed")
-        button.bind("<Button-1>", self.__register)
-        button.pack(side = tkinter.TOP, pady = 20)
+        self.disturbed_button = tkinter.Button(topFrame, height = 2, text = u"Being disturbed",
+                                               command = lambda : [self.__register(), self.__button_style_update(self.disturbed_button)])
+        self.disturbed_button.pack(side = tkinter.LEFT, padx = 10, pady = 10)
+
+        self.not_disturbed_button = tkinter.Button(topFrame, height = 2, text = u"Not being disturbed", state = tkinter.DISABLED,
+                                                   command = lambda : [self.__register(), self.__button_style_update(self.not_disturbed_button)])
+        self.not_disturbed_button.pack(side = tkinter.LEFT, padx = 10)
 
     def __add_history_display_area(self):
         bottomFrame = tkinter.LabelFrame(self.window, text = "History")
@@ -36,11 +40,20 @@ class MainDisplay:
         self.__scrolled_text = tkinter.scrolledtext.ScrolledText(master = bottomFrame)
         self.__scrolled_text.pack()
 
-    def __register(self, event):
+    def __register(self):
         now = datetime.datetime.now()
         text = str(now) + "\n"
         self.__io.write(text)
         self.__scrolled_text.insert("1.0", text)
+
+    def __button_style_update(self, button: tkinter.Button):
+        if button["text"] == "Being disturbed":
+            self.disturbed_button.configure(state = tkinter.DISABLED)
+            self.not_disturbed_button.configure(state = tkinter.NORMAL)
+        else:
+            self.disturbed_button.configure(state = tkinter.NORMAL)
+            self.not_disturbed_button.configure(state = tkinter.DISABLED)
+        self.window.update()
 
 
 disp = MainDisplay()
